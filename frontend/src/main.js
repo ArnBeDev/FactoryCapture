48c4f2dd5bd210a3ea385a748a114f9efad62a2a
ArnBeDev <bethge.arnd@gmail.com>
2023-01-04 18:34:28 +0100

    components added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,17 @@ import router from './router'
 import PrimeVue from 'primevue/config';
 import '@/assets/styles.scss';
 import Chart from 'primevue/chart';
+import Card from 'primevue/card';
+
 const app = createApp(App)
+
+
 
 app.use(router)
 app.use(PrimeVue, { ripple: true });
+
 app.component('Chart', Chart);
+app.component('Card', Card);
+
 
 app.mount('#app')
