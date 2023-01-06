6d648d5858c1eb0492a07cb3ecdf2a6bbc6667ad
ArnBeDev <bethge.arnd@gmail.com>
2023-01-06 17:27:44 +0100

    implemented machine data for dashboard

--- frontend/src/views/StartDashboard.vue
+++ frontend/src/views/StartDashboard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, reactive, ref, watch } from 'vue';
+import { onMounted, reactive, ReactiveEffect, ref, watch } from 'vue';
 import { useLayout } from '@/layout/composables/layout';
 import LiveChart from '@/components/LiveGraph.vue';
 
@@ -7,15 +7,15 @@ const { isDarkTheme, contextPath } = useLayout();
 
 const products = ref(null);
 
-let today = reactive({
+let machines =[];
+let loaded =false ;
 
-});
 
-let idleData =reactive({
 
-}
-);
 
+
+
+let liveData;
 
 const lineData = reactive({
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -48,6 +48,106 @@ const lineOptions = ref(null);
 onMounted(() => {
   
 });
+
+
+async function getData(){
+   
+    let err;
+  
+    axios.get('http://localhost:8080/api/livemachines').then((response) => {this.machines=response.data;createChartData();}).catch(err => {err=err})
+
+
+ 
+};
+
+function createChartData(){
+
+    let ids=[];
+    let idlePower=[];
+    let errorPower=[];
+    let workingPower=[];
+
+machines.sort(compare);
+
+machines.forEach(machine =>{
+ids.push(machine.machineId);
+
+if(machine.state ==2){
+//working
+workingPower.push(machine.power)
+errorPower.push(null);
+idlePower.push(null);
+
+
+}else if(machine.state <2){
+    //idle
+    workingPower.push(null)
+errorPower.push(machine.power);
+idlePower.push(null);
+
+
+}else {
+    //error
+    workingPower.push(null)
+errorPower.push(null);
+idlePower.push(machine.power);
+
+}
+
+})
+
+
+liveData = reactive({
+ labels: ids,
+ datasets:[
+{ //working
+    label:'working machines',
+   data:workingPower,
+   fill:true,
+   backgroundColor: '#00bb7e',
+            borderColor: '#00bb7e',
+
+},{
+    //idle
+    label:'idle machines',
+    data:idlePower,
+    fill:true,
+    backgroundColor: '#2f4860',
+            borderColor: '#2f4860',
+},{
+    //error
+    label:'error machines',
+    data:errorPower,
+    fill:true,
+    backgroundColor:'#900b0a',
+    borderColor:'#5c0002'
+}
+
+ ]
+
+ 
+
+
+});
+
+
+loaded=true;
+
+}
+
+
+function compare(a,b){
+      if(a.machineId < b.machineId){
+        return -1;
+      }
+      if(a.machineId >b.machineId){
+        return 1;
+      }
+      return 0;
+  
+    };
+
+
 
 const formatCurrency = (value) => {
     return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
@@ -131,7 +231,7 @@ watch(
             <div class="card">
                 <h5>Sales Overview</h5>
                 <Chart type="line" :data="lineData" :options="lineOptions" />
-               
+               <Chart v-if="loaded" type="bar" :data ="liveData"/>
               
 
 
