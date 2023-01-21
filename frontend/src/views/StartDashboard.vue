043214b28c518071f1d20a53157284e722e51d7c
ArnBeDev <bethge.arnd@gmail.com>
2023-01-21 20:22:34 +0100

    grid added to dashboard

--- frontend/src/views/StartDashboard.vue
+++ frontend/src/views/StartDashboard.vue
@@ -238,11 +238,11 @@ watch(
 
 <template>
     <div class="grid">
-        <div class="col-1 xl:col-6">
+        <div class="col-6">
 
 
 
-            <div class="card mb-0">
+            <div class="card">
                 <h5>Sales Overview {{ loaded }}</h5>
                 <Chart type="line" :data="lineData" :options="lineOptions" />
 
@@ -251,58 +251,29 @@ watch(
 
             </div>
 
+        </div>
 
 
 
-
-
-            <div class="card">
+<div class="col-6">
+    <div class="card">
                 <h5> Current power per Machine</h5>
                 <LiveMachine />
             </div>
 
 
+</div>
+           
 
 
 
 
         </div>
 
-    </div>
 
 
 
 
-
-
-
-
-    <div class="col-12 xl:col-6">
-        <div class="card">
-            <h5>Recent produced parts</h5>
-            <DataTable :value="products" :rows="5" :paginator="true" responsiveLayout="scroll">
-                <Column style="width: 15%">
-                    <template #header> Image </template>
-                    <template #body="slotProps">
-                        <img :src="contextPath + 'demo/images/product/' + slotProps.data.image"
-                            :alt="slotProps.data.image" width="50" class="shadow-2" />
-                    </template>
-                </Column>
-                <Column field="name" header="Name" :sortable="true" style="width: 35%"></Column>
-                <Column field="price" header="Price" :sortable="true" style="width: 35%">
-                    <template #body="slotProps">
-                        {{ formatCurrency(slotProps.data.price) }}
-                    </template>
-                </Column>
-                <Column style="width: 15%">
-                    <template #header> View </template>
-                    <template #body>
-                        <Button icon="pi pi-search" type="button" class="p-button-text"></Button>
-                    </template>
-                </Column>
-            </DataTable>
-        </div>
-    </div>
 
 
 
